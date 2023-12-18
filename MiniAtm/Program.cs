ad0ee0ed08c5b0f1e8348bf1564619f07fceb0ac
Asacer <148333712+AS-Acer@users.noreply.github.com>
2023-12-18 13:31:45 +0300

    Mini Atm upgrade
    a few minor changes

--- MiniAtm/Program.cs
+++ MiniAtm/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,54 +15,70 @@ namespace MiniAtm
             int bakiye = 10000;
 
             Console.WriteLine("Acerbanka hoşgeldiniz yapmak istediğiniz işlemi seçiniz ");
-            Console.WriteLine("1 -> bakiye görüntüleme"+ "2 -> para çekme "+ "3 -> para yatırma"+ "q -> çıkış yapma");
 
-            string secim = Console.ReadLine();
-            
-            
-
-            if (secim == "1")
+            while (true)
             {
-                Console.WriteLine("şuanki baliyeniz : " + bakiye);
-                Console.ReadLine();
+                Console.WriteLine("1 -> bakiye görüntüleme " + "2 -> para çekme " + "3 -> para yatırma " + "q -> çıkış yapma ");
 
-            }
-            else if (secim == "2")
-            {
-                Console.WriteLine("çekmek istediğiniz miktarı giriniz ");
-                int cıkanpara = Convert.ToInt32(Console.ReadLine());
-                if (cıkanpara<= bakiye)
-                { 
-                Console.WriteLine("çekilen miktar : " + cıkanpara);
-                Console.WriteLine("kalan bakiye : " + (bakiye - cıkanpara));
-                Console.ReadLine();
+                string secim = Console.ReadLine();
 
+
+
+                if (secim == "1")
+                {
+                    Console.WriteLine("şuanki baliyeniz : " + bakiye);
+                   
+                    Console.WriteLine("Bir üst menüye dönmek için bir tuşa basınız.");
+                    Console.ReadLine();
 
                 }
-                else { 
-                    Console.WriteLine("yetersiz bakiye");
+                else if (secim == "2")
+                {
+                    Console.WriteLine("çekmek istediğiniz miktarı giriniz ");
+                    int cıkanpara = Convert.ToInt32(Console.ReadLine());
+                    if (cıkanpara <= bakiye)
+                    {
+                        Console.WriteLine("çekilen miktar : " + cıkanpara);
+                        Console.WriteLine("kalan bakiye : " + (bakiye - cıkanpara));
+                        
+                        Console.WriteLine("Bir üst menüye dönmek için bir tuşa basınız.");
+                        Console.ReadLine();
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("yetersiz bakiye");
+                        
+                        Console.WriteLine("Bir üst menüye dönmek için bir tuşa basınız.");
+                        Console.ReadLine();
+                    }
+
+                }
+                else if (secim == "3")
+                {
+                    Console.WriteLine("yatırmak istediğiniz miktarı giriniz :");
+                    int girenpara = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Yatırmak istediğiniz parayı hazneye koyunuz ");
+                    Console.WriteLine("Mevcud bakiyeniz : "+ bakiye);
+                    Console.WriteLine("yatırmak istediğniz para : " + girenpara);
+                    Console.WriteLine("yeni bakiye : " + (bakiye + girenpara));
+                    Console.WriteLine("Bir üst menüye dönmek için bir tuşa basınız.");
+
                     Console.ReadLine();
                 }
-               
-            }
-            else if (secim == "3")
-            {
-                Console.WriteLine("yatırmak istediğiniz miktarı giriniz :");
-                int girenpara = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("yatırmak istediğiniz parayı şeye koyunuz ");
-                Console.WriteLine("şeydeki para : " + girenpara);
-                Console.WriteLine("kalan bakiye : " + (bakiye - girenpara));
-                Console.ReadLine();
-            }
-            else if (secim == "q")
-            {
-                Console.WriteLine("çıkış yapılıyor");
-                Console.WriteLine("çıkış yapıldı iyi günler ");
-                Console.ReadLine();
-            }
-            else {
-                Console.WriteLine("geçerli değer giriniz");
-                Console.ReadLine();
+                else if (secim == "q")
+                {
+                    Console.WriteLine("çıkış yapılıyor");
+                    Console.WriteLine("çıkış yapıldı iyi günler ");
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("geçerli değer giriniz");
+                    Console.ReadLine();
+                }
             }
         }
     }
